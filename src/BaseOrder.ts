b1e75b311b7c4bb50fd7aaba05169e2884a32d7a
Tomáš Hekrla <tomas@heky.eu>
2023-04-18 23:26:57 +0200

    feat: disallow to sell zero amount

--- src/BaseOrder.ts
+++ src/BaseOrder.ts
@@ -21,6 +21,9 @@ export class BaseOrder {
     public readonly sellingWallet: Wallet,
     public readonly buyingWallet: Wallet,
   ) {
+    if (config.sellingAmount <= 0) {
+      throw new Error('Selling amount must be positive');
+    }
     // reserve funds
     this.sellingWallet.withdraw(config.sellingAmount);
   }
