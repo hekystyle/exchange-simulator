50a58bbb2c390514779e6e9cb1842b3d5d8489ee
Tomáš Hekrla <tomas@heky.eu>
2023-04-21 18:28:47 +0200

    chore: use eslint-plugin-n

--- packages/frontend/vite.config.ts
+++ packages/frontend/vite.config.ts
@@ -1,4 +1,3 @@
-/* eslint-disable node/no-unpublished-import -- try to fix by installing eslint-plugin-n */
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
